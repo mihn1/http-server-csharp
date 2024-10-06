74a4ba639488039c597b2173b8ea0c6582f9c5b7
mihn <am141198@gmail.com>
2024-10-06 23:18:45 +0700

    fix user agent handler

--- src/Server.cs
+++ src/Server.cs
@@ -105,7 +105,7 @@ public class HttpServer
         }
         else if (uri == "/user-agent")
         {
-            var uAgent = message.Content?.Headers.GetValues("User-Agent").FirstOrDefault();
+            var uAgent = message.Headers.GetValues("User-Agent").FirstOrDefault();
             res.StatusCode = HttpStatusCode.OK;
             if (!string.IsNullOrWhiteSpace(uAgent))
                 res.Content = new StringContent(uAgent, new MediaTypeHeaderValue("text/plain"));
